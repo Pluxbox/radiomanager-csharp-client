46531cd84d410e87e6dc7cdc3a0f768809ddd199
siebe <siebe@pluxbox.nl>
2017-08-14 17:13:46 +0200

    Release of version 1.0.94

--- docs/StoryDataInput.md
+++ docs/StoryDataInput.md
@@ -8,7 +8,6 @@ Name | Type | Description | Notes
 **FieldValues** | **Object** |  | [optional] 
 **Name** | **string** |  | 
 **Description** | **string** |  | [optional] 
-**Tags** | **List&lt;int?&gt;** |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
--- src/RadioManager/Model/StoryDataInput.cs
+++ src/RadioManager/Model/StoryDataInput.cs
@@ -43,8 +43,7 @@ namespace RadioManager.Model
         /// <param name="FieldValues">FieldValues.</param>
         /// <param name="Name">Name (required).</param>
         /// <param name="Description">Description.</param>
-        /// <param name="Tags">Tags.</param>
-        public StoryDataInput(long? ModelTypeId = default(long?), bool? Recommended = default(bool?), Object FieldValues = default(Object), string Name = default(string), string Description = default(string), List<int?> Tags = default(List<int?>))
+        public StoryDataInput(long? ModelTypeId = default(long?), bool? Recommended = default(bool?), Object FieldValues = default(Object), string Name = default(string), string Description = default(string))
         {
             // to ensure "ModelTypeId" is required (not null)
             if (ModelTypeId == null)
@@ -67,7 +66,6 @@ namespace RadioManager.Model
             this.Recommended = Recommended;
             this.FieldValues = FieldValues;
             this.Description = Description;
-            this.Tags = Tags;
         }
         
         /// <summary>
@@ -101,12 +99,6 @@ namespace RadioManager.Model
         public string Description { get; set; }
 
         /// <summary>
-        /// Gets or Sets Tags
-        /// </summary>
-        [DataMember(Name="tags", EmitDefaultValue=false)]
-        public List<int?> Tags { get; set; }
-
-        /// <summary>
         /// Returns the string presentation of the object
         /// </summary>
         /// <returns>String presentation of the object</returns>
@@ -119,7 +111,6 @@ namespace RadioManager.Model
             sb.Append("  FieldValues: ").Append(FieldValues).Append("\n");
             sb.Append("  Name: ").Append(Name).Append("\n");
             sb.Append("  Description: ").Append(Description).Append("\n");
-            sb.Append("  Tags: ").Append(Tags).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
         }
@@ -180,11 +171,6 @@ namespace RadioManager.Model
                     this.Description == other.Description ||
                     this.Description != null &&
                     this.Description.Equals(other.Description)
-                ) && 
-                (
-                    this.Tags == other.Tags ||
-                    this.Tags != null &&
-                    this.Tags.SequenceEqual(other.Tags)
                 );
         }
 
@@ -209,8 +195,6 @@ namespace RadioManager.Model
                     hash = hash * 59 + this.Name.GetHashCode();
                 if (this.Description != null)
                     hash = hash * 59 + this.Description.GetHashCode();
-                if (this.Tags != null)
-                    hash = hash * 59 + this.Tags.GetHashCode();
                 return hash;
             }
         }
